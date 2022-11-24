793056c5b28c50eb5c1dac1a445ab9a2c873ef68
Md Fahim Faisal <mdfahimfaisal000@gmail.com>
2022-11-24 12:43:18 +0600

    load categories collection

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
 const app = express();
 
@@ -12,6 +14,31 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Hello from resale.com server');
 });
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tzinyke.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+
+    const categoriesCollection = client.db('carResale').collection('categories');
+    const productsCollection = client.db('carResale').collection('products');
+
+    try {
+        app.get('/categories', async (req, res) => {
+            const query = {};
+            const collections = await categoriesCollection.find(query).toArray();
+            res.send(collections);
+        });
+    }
+    finally {
+
+    }
+}
+
+
+run().catch(err => console.error(err));
+
 
 app.listen(port, () => {
     console.log('resale.com is running on port :', port);
