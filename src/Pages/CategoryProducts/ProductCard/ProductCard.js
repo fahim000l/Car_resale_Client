b563904698a1ecf533092f2a2f2dcdc1dcd771c4
Md Fahim Faisal <mdfahimfaisal000@gmail.com>
2022-11-24 22:30:09 +0600

    seller verification tick

--- src/Pages/CategoryProducts/ProductCard/ProductCard.js
+++ src/Pages/CategoryProducts/ProductCard/ProductCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { CheckCircleIcon } from '@heroicons/react/24/solid';
 
 const ProductCard = ({ product }) => {
 
@@ -11,20 +12,27 @@ const ProductCard = ({ product }) => {
         postedTime,
         sellerName,
         isVerified,
-        categoryName,
-        categoryId,
         picture
     } = product;
 
     return (
         <div className="card card-compact bg-violet-400 shadow-xl">
             <figure><img className='w-full h-[300px]' src={picture} alt="Shoes" /></figure>
-            <div className="card-body">
-                <h2 className="card-title">{carName}</h2>
-                <p>{categoryName}</p>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
+            <div className="card-body text-start text-black">
+                <h2 className="card-title text-2xl font-bold">{carName}</h2>
+                <p className='text-xl'>Resale Price : <span className='font-bold'>$ {resalePrice}</span></p>
+                <p className='text-xl'>Original Price : <span className='font-bold'>$ {originalPrice}</span></p>
+                <p className='text-xl'>years of use: {yearOfUse}</p>
+            </div>
+            <div className='flex items-center justify-between px-5 py-2'>
+                <div className='text-start'>
+                    <p className='text-xl text-black'>Seller Name: {sellerName}</p>
+                    <p className='text-xl text-black'>Location : {location}</p>
+                    <p className='text-xl text-black'>Posted Date : {postedTime}</p>
                 </div>
+                {
+                    isVerified && <CheckCircleIcon className="h-[60px] w-[60px] text-blue-600" />
+                }
             </div>
         </div>
     );
